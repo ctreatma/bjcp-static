6a71190a861ee695544bbd9ba41e5e0378157315
Charles Treatman <charles.treatman@gmail.com>
2015-04-24 14:44:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+BJCP-Static
+===========
+
+BJCP-Static is a JSON API for BJCP style guide data.  The BJCP style guide is not updated often (the current version was released in 2008), it's a small dataset, and it's likely to be duplicated in all or most beer-related applications, so it lends itself well to being distributed as static files. 
---
+++ generate_api.rb
@@ -0,0 +1,20 @@
+require './styles'
+require 'json'
+
+index = []
+
+STYLES.each do |style_group|
+  style_group[:styles_attributes].each do |style|
+    id = "#{style_group[:number]}#{style[:letter]}"
+    style.delete(:letter)
+    style[:id] = id
+    index << style
+    file = File.open("styles/#{id}.json", 'w')
+    file.write(style.to_json)
+    file.close
+  end
+end
+
+file = File.open('styles/index.json', 'w')
+file.write(index.to_json)
+file.close
